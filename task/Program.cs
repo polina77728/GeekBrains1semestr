0b4f4681ac7a8c6522feb33bb605e681d50bc4da
Polina Balova <polina77728@yandex.ru>
2022-12-27 11:10:13 +0300

    Решение  задания

--- task/Program.cs
+++ task/Program.cs
@@ -3,3 +3,33 @@
 [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → [] */
+string[] array = new string[5] {"Hello", "2", "world", ":-)" , "456" };
+string[] array2 = new string[array.Length];
+
+int length = 3;
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= length)
+    {
+        array2[count] = array[i];
+        count++;
+    }
+}
+
+Console.WriteLine();
+WriteArray(array);
+Console.WriteLine();
+WriteArray(array2);
+
+
+
+void WriteArray(string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
